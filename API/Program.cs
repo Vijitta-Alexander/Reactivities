0448b200f0421ef95b13a599ac16a9de7e1f64f3
VijittaAlexander <vijitta.alexander@kanini.com>
2022-12-06 15:45:25 +0530

    first commit

---
+++ API/Program.cs
@@ -0,0 +1,44 @@
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<DataContext>(opt => 
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DatabaseConnection")));
+
+var app = builder.Build();
+
+var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+
+try{
+    var context = services.GetRequiredService<DataContext>();
+    context.Database.Migrate();
+    await Seed.SeedData(context);
+}
+catch(Exception ex) {
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex,"Error occured during migration");
+
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
